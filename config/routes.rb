5a1f45c3f69ccf9b1cb1b0cca7c2198d707ae831
okothkongo <okotho@students.ku.ac.ke>
2018-12-18 12:29:12 +0300

    add performance create route

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
   namespace :v0 do
-    resources :pings, only: [:index], constraints: { format: 'json' }
-    end
-  namespace :v1 do
+     resources :pings, only: [:index], constraints: { format: 'json' }
+  end
+  namespace :v1, default: {format: :json}do
     mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+    resources :performance_data, only: [:create]
     end
   end
 end
